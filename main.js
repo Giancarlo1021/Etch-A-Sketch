a2c3a96c276b18ffe3130de563c86cf2fef56ba1
giancarlo1021 <giancarloavila725@gmail.com>
2021-09-06 08:37:44 -0700

    completed

--- main.js
+++ main.js
@@ -1,14 +1,63 @@
-const grid = () => { 
-        for (var i = 0; i < 4; i++) {
-            var row = document.createElement('div');
-            row.className = "row";
-            for (var j = 0; j < 4; j++) {
-                var box = document.createElement('div');
-                box.className = "box";
-                row.appendChild(box);
-            }                
-            document.getElementById('boxParent').appendChild(row);
-        }
-    }
+const container = document.querySelector("#container");
+const clearBtn = document.querySelector("#clear-btn");
+const resizeBtn = document.querySelector("#resize-btn");
 
-grid();
+const getRandomColor = () => {
+    var variables = '0123456789ABCDEF'
+    var chosenColor = '#'
+    for (var i = 0; i < 6; i++) {
+        chosenColor += variables[Math.floor(Math.random() * 16)];
+    }
+    return chosenColor;
+}
+
+const grid = (e) => {
+    for (var i = 0; i < e; i++) {
+        var row = document.createElement('div');
+        row.className = "row";
+        for (var j = 0; j < e; j++) {
+            var box = document.createElement('div');
+            box.style.width = `${520 / e}px`;
+            box.style.height = `${520 / e}px`;
+            box.className = "box";
+            row.appendChild(box);
+        }
+        container.appendChild(row);
+    }
+}
+
+
+clearBtn.addEventListener('click', () => {
+    for (let i = 0; i < boxDiv.length; i++) {
+        boxDiv[i].style.backgroundColor = "grey";
+    }
+})
+
+resizeBtn.addEventListener('click', () => {
+    for (let i = 0; i < rowDiv.length; ++i) {
+        rowDiv[i].parentNode.removeChild(rowDiv[i]); // removes elements in html
+    }
+    let cellQuestion = prompt('Please chose how you would like to resize your grid')
+    let cellNum = parseInt(cellQuestion, 10);
+    grid(cellNum)
+    setColors();
+ setRow();
+});
+
+let setColors = () => {
+    boxDiv = document.querySelectorAll(".box");
+    for (let i = 0; i < boxDiv.length; i++) {
+        boxDiv[i].addEventListener("mouseover", () => {
+            boxDiv[i].style.backgroundColor = getRandomColor();
+        })
+    }
+}
+
+let setRow = () => {
+    rowDiv = document.querySelectorAll(".row");
+}
+
+grid(4);
+let boxDiv = document.querySelectorAll(".box");
+let rowDiv = document.querySelectorAll(".row");
+setColors();
